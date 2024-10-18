07e35000e1228de026dc92bd55a6424edc4ecc5e
hiether17 <lhietherfhiona@gmail.com>
2024-10-18 22:39:46 +0800

    Added class AppletGallery

--- applet gallery.js
+++ applet gallery.js
@@ -20,3 +20,35 @@ class AppletGallery {
             console.error('Error fetching data:', error);
         }
     }
+    renderAppletGallery(appletgallery) {
+        const appletgalleryCont = document.getElementById('appletgalleryCont');
+        appletgalleryCont.innerHTML = appletgallery.map(applet => 
+            `<div class="card" style="width: 18rem;"> 
+               <img src = "${applet.Image}" class="card-img-top image" alt="${applet.Applet_No}">
+                    <h5> ${applet.Applet_No} </h5>
+                    <p> ${applet.Description}</p>
+                    <a href = "${applet.file}" class = "btn btn-primary">Go to Applet </a>
+            </div>`
+        ).join('');
+    }
+
+    bindSearchEvent() {
+        const appletSearchBar = document.getElementById('appletSearchBar'); 
+
+        appletSearchBar.addEventListener('input', () => {
+            this.filterApplet(appletSearchBar.value);
+        });
+
+    }
+
+    filterApplet(query) {
+        const filteredapplet = this.appletgallery.filter(applet => {
+            return applet.Applet_No.toLowerCase().includes(query.toLowerCase())
+        });
+
+        this.renderAppletGallery(filteredapplet);
+    }
+    
+}
+
+const appletgallery = new AppletGallery('applet.json');
