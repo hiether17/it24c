295f1266f128e04fa474c6765585f37c7942e433
IT24CSALVO <alliahshanesalvo11@gmail.com>
2024-11-03 17:36:51 +0800

    added an weather card and Event listener

--- applet-5.js
+++ applet-5.js
@@ -9,5 +9,19 @@ class WeatherApp {
 
         // Geolocation Input
         this.getLocationBtn = document.getElementById('getLocationBtn');
+
+         // Weather Card
+         this.weatherCard = document.getElementById('weatherCard');
+         this.cityName = document.getElementById('cityName');
+         this.temperature = document.getElementById('temperature');
+         this.description = document.getElementById('description');
+         this.humidity = document.getElementById('humidity');
+         this.windSpeed = document.getElementById('windSpeed');
+ 
+         // Event Listener
+         this.getWeatherBtn.addEventListener('click', () => this.fetchWeather());
+         this.getLocationBtn.addEventListener('click', () => this.fetchWeatherByLocation());
+     }
+ 
     }
-}
+
